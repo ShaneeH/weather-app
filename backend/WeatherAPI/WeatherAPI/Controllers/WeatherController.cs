6f2a2598ad63891fcbbad9c235a67f4b8f5a6fb5
Shane Hahesy <hahesyshane@gmail.com>
2026-03-25 01:45:25 +0000

    Add WeatherController with basic GET endpoint

---
+++ backend/WeatherAPI/WeatherAPI/Controllers/WeatherController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "API is running";
+        }
+    }
+}
